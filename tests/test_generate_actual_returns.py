07bf5aa3eec070035c646194cf9fe6f14b1bd936
Forrest Surles <85535986+ForrestSurles@users.noreply.github.com>
2021-10-13 19:58:33 -0500

    Add to test_generate_actual_returns

--- tests/test_generate_actual_returns.py
+++ tests/test_generate_actual_returns.py
@@ -15,9 +15,32 @@ functions provided by the file.
 """
 
 # Imports
+import pandas as pd
+
 from pathlib import Path
+
 from challenge.custom_functions import generate_actual_returns
 
-# test function - generate_actual_returns
+# test csv import and calculation of closing price actual returns
 def test_generate_actual_returns():
+
     input_csv = Path('../Resources/emerging_markets_ohlcv.csv')
+    checker_csv = Path('../Resources/test_results_generate_actual_returns.py')
+
+    input_df = pd.read_csv(
+        input_csv,
+        index_col='date',
+        infer_datetime_format=True,
+        parse_dates=True
+        )
+
+    checker_df = pd.read_csv(
+        checker_csv_
+        index_col='date',
+        infer_datetime_format=True,
+        parse_dates=True
+        )
+
+
+    attempt generate_actual_returns(input_df,,True) == checker_df
+
